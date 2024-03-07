6a08ca114a42fc8f84d64a14dab0968fdcf6ddec
ThibautDeduit <misterdi@protonmail.com>
2024-03-07 08:46:37 +0100

    Create minimal console version

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+let board = [' ', ' ', ' ',
+			 ' ', ' ', ' ', 
+			 ' ', ' ', ' ']
+
+function displayBoard () {
+	console.log(board.slice(0,3))
+	console.log(board.slice(3,6))
+	console.log(board.slice(6,9))
+}
+function markCellX(cell) {
+	board[cell] = "X"
+	displayBoard()
+}
+function markCellO(cell) {
+	board[cell] = "O"
+	displayBoard()
+}
